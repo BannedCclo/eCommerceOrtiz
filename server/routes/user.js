51d28c1cdb1984d525855133a3ed5ca77ce486fd
BannedCclo <cecelokun@gmail.com>
2024-10-29 18:19:27 -0300

    Alterações jwt

--- server/routes/user.js
+++ server/routes/user.js
@@ -1,29 +1,43 @@
-const express = require('express');
-const User = require('../model/user');
+const express = require("express");
+const User = require("../model/user");
 const router = express.Router();
+const jwt = require("jsonwebtoken");
 
-router.get('/test', ()=>{
-    console.log('requisitou a api aq')
-})
+router.get("/test", () => {
+  console.log("requisitou a api aq");
+});
 
-router.get('/users', async (req, res) => {
+router.get("/users", async (req, res) => {
   const userArr = await User.findAll().catch((err) => console.log(err));
   res.json(userArr);
   console.log(userArr);
 });
 
-router.get('/user', async (req, res) => {
-  const id = req.params.id;
+router.post("/login", async (req, res) => {
+  const { email, password } = req.body;
   const userArr = await User.findAll();
-  const user = userArr.find((user) => user.id === id);
-  res.json(user);
+  const user = userArr.find((user) => user.email === email);
+
+  if (user.password === password) {
+    const payload = {
+      id: user.id,
+      email: user.email,
+      // Outros campos que você quiser incluir no token
+    };
+
+    const token = jwt.sign(payload, "senhaSuperSecreta", { expiresIn: "1h" });
+
+    console.log(token);
+
+    res.json(token);
+  }
 });
 
-router.post('/user', async (req, res) => {
+router.post("/user", async (req, res) => {
   const { username, email, password, type } = req.body;
-  const newUser = await User.create({ username, email, password, type});
+  const newUser = await User.create({ username, email, password, type });
   res.json(newUser);
   console.log(newUser);
 });
 
-module.exports =  router;
+module.exports = router;
--- web/src/pages/login/index.tsx
+++ web/src/pages/login/index.tsx
@@ -1,59 +1,64 @@
-
-import styles from './Login.module.css'
-import {useNavigate} from 'react-router-dom'
-import api from '../../service'
-import {useState} from 'react'
+import styles from "./Login.module.css";
+import { useNavigate } from "react-router-dom";
+import api from "../../service";
+import { useState } from "react";
 
 export interface User {
   id: number;
   username: string;
   email: string;
   password: string;
-  
 }
 
 function Login() {
+  const navigate = useNavigate();
+  const [userEmail, setUserEmail] = useState("");
+  const [userPassword, setUserPassword] = useState("");
 
-    const navigate = useNavigate()
-    const [userEmail, setUserEmail] = useState('')
-    const [userPassword, setUserPassword] = useState('')
+  function login() {
+    try {
+      api.post("/login", {
+        email: userEmail,
+        password: userPassword,
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  }
 
-      function login() {
-        try {
-          api.get('/users').then((res)=>{
-            const arr = res.data
-            const user = arr.filter((user: User)=> user.email === userEmail && user.password === userPassword)
-            if(user.length === 0){
-              alert('Usuário ou senha incorretos')
-              return
-            } else {
-              console.log(user[0])
-            }
-            
-          })  
-        }catch (error) {
-          console.log(error)
-        }   
-      }
-
-    return (
-      <div className={styles.container}>
+  return (
+    <div className={styles.container}>
       <div id={styles.loginCard}>
         <h1>Login</h1>
         <div className={styles.inputContainer}>
           <h1>Email:</h1>
-          <input type="text" value={userEmail} onChange={(e)=>{setUserEmail(e.target.value)}}/>
+          <input
+            type="text"
+            value={userEmail}
+            onChange={(e) => {
+              setUserEmail(e.target.value);
+            }}
+          />
         </div>
         <div className={styles.inputContainer}>
           <h1>Senha:</h1>
-          <input type="text" value={userPassword} onChange={(e)=>{setUserPassword(e.target.value)}}/>
+          <input
+            type="text"
+            value={userPassword}
+            onChange={(e) => {
+              setUserPassword(e.target.value);
+            }}
+          />
         </div>
-        <button id={styles.loginButton} onClick={login}>Enviar</button>
-        <h2 id={styles.signUp} onClick={() => navigate('signUp')}>Criar uma conta</h2>
+        <button id={styles.loginButton} onClick={login}>
+          Enviar
+        </button>
+        <h2 id={styles.signUp} onClick={() => navigate("signUp")}>
+          Criar uma conta
+        </h2>
       </div>
-      </div>
-    )
-  
+    </div>
+  );
 }
 
-export default Login
+export default Login;
